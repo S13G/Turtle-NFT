53896d4a9ad57c1248f32ba82d492be5cc1cfe72
s13g <ayflix0@gmail.com>
2022-10-05 13:02:59 +0100

    done with templating

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from store.views import marketplace, ProductDetail, category_view, CategoryList, search_view, main_page
+from store.views import marketplace, category_view, CategoryList, search_view, main_page
 
 
 urlpatterns = [
     path('', main_page, name="main-page"),
     path('marketplace/', marketplace, name='marketplace'),
-    path('product/<slug:slug>/', ProductDetail.as_view(), name='product-detail'),
     path('category/', CategoryList.as_view(), name="category"),
     path('category/<slug:slug>', category_view, name='category-filter'),
     path('search/', search_view, name="search")
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from setting.models import Settings
 from store.models import Product, Category
 
@@ -16,10 +16,6 @@ class CategoryList(ListView):
     context_object_name = 'category'
 
 
-class ProductDetail(DetailView):
-    model = Product
-    template_name = 'store/product-detail.html'
-
 # Product Listing
 def marketplace(request):
     products = Product.objects.select_related('category').all()
@@ -27,34 +23,39 @@ def marketplace(request):
     context = {"products": products, "categories": categories}
     return render(request, 'store/marketplace.html', context)
 
+
 # Category filter
 def category_view(request, slug):
+    categories = Category.objects.all()
     try:
         category = Category.objects.get(slug=slug)
     except Category.DoesNotExist:
         messages.warning("No Such Category")
     products = category.products.all()
-    context = {"products": products, "category": category}
-    return render(request, "store/product-filter.html", context)
+    context = {"products": products,
+               "categories": categories, "category": category}
+    return render(request, "store/marketplace-filter.html", context)
+
 
 # search query
 def search_view(request):
+    categories = Category.objects.all()
     results = []
     if request.method == "GET":
-        query = request.GET.get("")
+        query = request.GET.get("search")
         if query == '':
             query = 'None'
-        results = Product.objects.filter(Q(name__icontains=query) | Q(price__icontains=query) | Q(description__icontains=query))
-    context = {"query": query, "results": results}
-    return render(request, 'store/search.html', context)
+        results = Product.objects.filter(Q(name__icontains=query) | 
+        Q(price__icontains=query) | Q(token__icontains=query))
+    context = {"query": query, "results": results, "categories": categories}
+    return render(request, 'store/search-item.html', context)
 
 
 def main_page(request):
     Settings.objects.get_or_create(id=1)
-    
+
     context = {"settings": Settings.objects.first()}
 
     if context['settings']:
         os.environ['web_name'] = context['settings'].trade_mark
     return render(request, 'base.html')
-   
